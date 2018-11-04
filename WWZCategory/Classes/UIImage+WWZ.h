bbc90c3ee015827d19b7657e4d6a6cc4bf49e924
王伍州 <ccwuzhou@icloud.com>
2018-11-04 13:10:54 +0800

    1.0.3

--- WWZCategory/Classes/UIImage+WWZ.h
+++ WWZCategory/Classes/UIImage+WWZ.h
@@ -8,7 +8,30 @@
 
 #import <UIKit/UIKit.h>
 
+typedef NS_ENUM(NSUInteger, GradientType) {
+    topToBottom = 0,//从上到小
+    leftToRight = 1,//从左到右
+    upleftTolowRight = 2,//左上到右下
+    uprightTolowLeft = 3,//右上到左下
+};
+
 @interface UIImage (WWZ)
+
++ (instancetype)wwz_imageWithImage:(UIImage *)image size:(CGSize)size;
+/**
+ *  由二维码生成图片
+ */
++ (instancetype)wwz_imageWithQRCode:(NSString *)qrcode size:(CGSize)size;
+/**
+ *  从图片中获取url
+ */
++ (NSString *)wwz_imageMessageString:(UIImage *)image;
+
++ (instancetype)wwz_imageWithColor:(UIColor *)color size:(CGSize)size alpha:(float)alpha;
+/**
+ 获取矩形的渐变色的UIImage
+ */
++ (instancetype)wwz_gradientImageWithSize:(CGSize)size colors:(NSArray *)colors gradientType:(GradientType)gradientType;
 
 + (instancetype)wwz_imageWithImage:(UIImage *)image angle:(CGFloat )angle;
 
