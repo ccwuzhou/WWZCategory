3e1ce9312577071ae74702a782c7c59ac3d84c63
apple <apple@appledeimac.local>
2018-12-06 12:34:31 +0800

    观察者

--- WWZCategory/Classes/NSObject+WWZ.m
+++ WWZCategory/Classes/NSObject+WWZ.m
@@ -32,4 +32,105 @@
     free(properties);
     return propsDic;
 }
+
++ (void)load{
+    [self switchMethod];
+}
+
++ (void)switchMethod{
+    method_exchangeImplementations(class_getClassMethod([self class], @selector(addObserver:forKeyPath:options:context:)),
+                                   class_getClassMethod([self class], @selector(wwz_addObserver:forKeyPath:options:context:)));
+    method_exchangeImplementations(class_getClassMethod([self class], @selector(removeObserver:forKeyPath:context:)),
+                                   class_getClassMethod([self class], @selector(wwz_removeObserver:forKeyPath:context:)));
+}
+
+
+#pragma mark - 第一种方案，利用@try @catch
+//// 交换后的方法
+//- (void)removeDasen:(NSObject *)observer forKeyPath:(NSString *)keyPath
+//{
+//    @try {
+//        [self removeDasen:observer forKeyPath:keyPath];
+//    } @catch (NSException *exception) {}
+//
+//}
+
+//// 交换后的方法
+//- (void)addDasen:(NSObject *)observer forKeyPath:(NSString *)keyPath options:(NSKeyValueObservingOptions)options context:(void *)context
+//{
+//    [self addDasen:observer forKeyPath:keyPath options:options context:context];
+//
+//}
+
+#pragma mark - 第二种方案，利用私有属性
+//// 交换后的方法
+//- (void)removeDasen:(NSObject *)observer forKeyPath:(NSString *)keyPath
+//{
+//    NSMutableArray *Observers = [DSObserver sharedDSObserver];
+//    ObserverData *userPathData = [self observerKeyPath:keyPath];
+//    // 如果有该key值那么进行删除
+//    if (userPathData) {
+//        [Observers removeObject:userPathData];
+//        [self removeDasen:observer forKeyPath:keyPath];
+//    }
+//    return;
+//}
+//
+//// 交换后的方法
+//- (void)addDasen:(NSObject *)observer forKeyPath:(NSString *)keyPath options:(NSKeyValueObservingOptions)options context:(void *)context
+//{
+//    ObserverData *userPathData= [[ObserverData alloc]initWithObjc:self key:keyPath];
+//    NSMutableArray *Observers = [DSObserver sharedDSObserver];
+//
+//    // 如果没有注册，那么才进行注册
+//    if (![self observerKeyPath:keyPath]) {
+//        [Observers addObject:userPathData];
+//        [self addDasen:observer forKeyPath:keyPath options:options context:context];
+//    }
+//
+//}
+//
+//// 进行检索，判断是否已经存储了该Key值
+//- (ObserverData *)observerKeyPath:(NSString *)keyPath
+//{
+//    NSMutableArray *Observers = [DSObserver sharedDSObserver];
+//    for (ObserverData *data in Observers) {
+//        if ([data.objc isEqual:self] && [data.keyPath isEqualToString:keyPath]) {
+//            return data;
+//        }
+//    }
+//    return nil;
+//}
+
+#pragma mark - 第三种方案，利用私有属性
+// 交换后的方法
+- (void)wwz_removeObserver:(NSObject *)observer forKeyPath:(NSString *)keyPath context:(void *)context{
+    if ([self observerKeyPath:keyPath observer:observer]) {
+        [self wwz_removeObserver:observer forKeyPath:keyPath context:context];
+    }
+}
+
+// 交换后的方法
+- (void)wwz_addObserver:(NSObject *)observer forKeyPath:(NSString *)keyPath options:(NSKeyValueObservingOptions)options context:(void *)context{
+    if (![self observerKeyPath:keyPath observer:observer]) {
+        [self wwz_addObserver:observer forKeyPath:keyPath options:options context:context];
+    }
+}
+
+// 进行检索获取Key
+- (BOOL)observerKeyPath:(NSString *)key observer:(id )observer{
+    id info = self.observationInfo;
+    NSArray *array = [info valueForKey:@"_observances"];
+    for (id objc in array) {
+        id Properties = [objc valueForKeyPath:@"_property"];
+        id newObserver = [objc valueForKeyPath:@"_observer"];
+        
+        NSString *keyPath = [Properties valueForKeyPath:@"_keyPath"];
+        if ([key isEqualToString:keyPath] && [newObserver isEqual:observer]) {
+            return YES;
+        }
+    }
+    return NO;
+}
+
 @end
